ab010ff9481e33206a9018bd434bcb31c68af9bf
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-10-22 15:34:49 +0400

    Menu components added

--- coffee-site/src/App.jsx
+++ coffee-site/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Features from "./components/features/Features";
+import Menu from "./components/menu/Menu";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Features />
+      <Menu />
     </>
   );
 }
